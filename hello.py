29ea26f1b11dc345c6b1e3c6df62b3e1a1cc1ef2
Livio <liviofeler971@outlook.fr>
2018-07-31 14:05:25 +0200

    validation quatrième point

--- hello.py
+++ hello.py
@@ -1,13 +1,12 @@
 #! /usr/bin/python
 # -*- coding:utf-8 -*-
 
-from flask import Flask
+from flask import Flask,render_template
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Hello Word !"
+    return render_template('helloWorld.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
-
