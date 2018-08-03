8f4c7b1d18cb4ec8e8740609b5990684f9848b4e
Quentin Pleplé <quentin.pleple@gmail.com>
2018-08-03 14:30:53 +0200

    commentaires

--- config.py
+++ config.py
@@ -1,3 +1,9 @@
+#/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+#Q ce fichier ne doit pas être git !
+#Q exporter la structure de la base de données "Robot" au format SQL et l'enregistrer dans le projet sous "schema.sql"
+
 SECRET_KEY = "6cad)jz88zm5gv+4)!1)9q_kq#-9o7_e+!x%h@oyi=vo2gt#ej"
 DATABASE_NAME = "Robot"
 DATABASE_USER = 'root'
--- robotapp/views.py
+++ robotapp/views.py
@@ -1,5 +1,6 @@
 #/usr/bin/env python
 # -*- coding:utf-8 -*-
+
 from flask import Flask,render_template, request
 from torndb import Connection
 
@@ -8,22 +9,31 @@ app.config.from_object('config')
 
 db = Connection('127.0.0.1', app.config['DATABASE_NAME'], app.config['DATABASE_USER'], app.config['DATABASE_PASS'])
 
+#Q convention de code PEP8 : https://www.python.org/dev/peps/pep-0008/
+
 @app.route('/')
 def index():
+    
+    #Q JAMAIS de return de string !
     return "Hello world !"
 
 @app.route('/formulaire/', methods=['GET','POST'])
 def formulaire():
     if request.method == 'GET':
-        return  render_template ('formulaire.html')
+        return render_template ('formulaire.html')
     else:
         msg = request.form['textarea']
         db.execute("INSERT INTO Test (test) VALUES (%s)",msg)
+
+        #Q pas de return de string
         return "Vous avez envoyé: {msg}".format(msg=request.form['textarea'])
 
 @app.route('/SQLHelloWorld')
 def SQLHelloWorld():
-    post = db.get("Select test From Test")
+    #Q en mysql les mots clés sont en majuscules
+    post = db.get("SELECT test FROM Test")
+
+    #Q pas de return de string
     return post.test
 
 
