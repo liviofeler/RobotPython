4db198e3d8c1d05c8f8f8a0440d8992844f93019
Livio <liviofeler971@outlook.fr>
2018-08-03 12:20:47 +0200

    03/08/2018 12:20

--- robotapp/views.py
+++ robotapp/views.py
@@ -6,6 +6,8 @@ from torndb import Connection
 app = Flask(__name__)
 app.config.from_object('config')
 
+db = Connection('127.0.0.1', app.config['DATABASE_NAME'], app.config['DATABASE_USER'], app.config['DATABASE_PASS'])
+
 @app.route('/')
 def index():
     return "Hello world !"
@@ -15,12 +17,12 @@ def formulaire():
     if request.method == 'GET':
         return  render_template ('formulaire.html')
     else:
+        msg = request.form['textarea']
+        db.execute("INSERT INTO Test (test) VALUES (%s)",msg)
         return "Vous avez envoyé: {msg}".format(msg=request.form['textarea'])
 
 @app.route('/SQLHelloWorld')
 def SQLHelloWorld():
-    db = Connection('127.0.0.1',app.config['DATABASE_NAME'], app.config['DATABASE_USER'], app.config['DATABASE_PASS'])
-
     post = db.get("Select test From Test")
     return post.test
 
